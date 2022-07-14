365531934259ea0f555323096553451749b1e54f
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-07-15 00:12:02 +0200

    76 Maquetando el registro de usuario

--- src/componentes/RegistroUsuarios.js
+++ src/componentes/RegistroUsuarios.js
@@ -1,8 +1,62 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+import {Header, Titulo, ContenedorHeader} from './../elementos/Header';
+import Boton from './../elementos/Boton'
+import {Formulario, Input, ContenedorBoton} from './../elementos/ElementosDeFormulario';
+
+import {ReactComponent as SvgLogin} from './../imagenes/registro.svg';
+import styled from "styled-components";
+
+const Svg = styled(SvgLogin)`
+	width: 100%;
+	max-height: 6.25rem; /* 100px */
+	margin-bottom: 1.25rem; /* 20px */
+`;
+
 
 const RegistroUsuarios = () => {
     return ( 
-        <h1>Registro Usuarios</h1>
+        <>
+            <Helmet>
+                <title>Crear Cuenta</title>
+            </Helmet>
+
+            <Header>
+                <ContenedorHeader>
+                    <Titulo>Crear Cuenta</Titulo>
+                    <div>
+                        <Boton to="/iniciar-sesion">Iniciar Sesion</Boton>
+                    </div>
+                </ContenedorHeader>
+            </Header>
+            <Formulario>
+            <Svg />
+                <Input 
+                    type="email"
+                    name="email"
+                    placeholder="Correo Electrónico"
+                />
+                <Input 
+                    type="password"
+                    name="password"
+                    placeholder="Contraseña"
+                />
+
+                <Input 
+                    type="password"
+                    name="password2"
+                    placeholder="Repetir la Contraseña"
+                />
+                <ContenedorBoton>
+                    <Boton 
+                    primario
+                    as="button"
+                    type="submit"
+                    >Crear Cuenta</Boton>
+                </ContenedorBoton>
+
+            </Formulario>
+        </>
      );
 }
  
